b3c94e36d39f8289bd0a65ec3b1ab4ae9e9a4e3c
Snaylaker <mehdihimmid73@gmail.com>
2024-12-15 17:02:41 +0100

    exo 00

--- Mehdi/j-00/exo00/hello.py
+++ Mehdi/j-00/exo00/hello.py
@@ -3,7 +3,16 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
-ft_list[2] = "world"
+ft_list[1] = "World"
+
+lst_tuple = list(ft_tuple)
+lst_tuple[1] = "France"
+ft_tuple = tuple(lst_tuple)
+
+ft_set.add("Paris")
+ft_set.discard("tutu!")
+
+ft_dict["Hello"] = "42Paris"
 
 print(ft_list)
 print(ft_tuple)
