04ee9823a385f02fe1344e7c20be4a56a0510560
Snaylaker <mehdihimmid73@gmail.com>
2025-02-16 17:52:33 +0100

    day1: exo00

---
+++ Mehdi/j-01/exo-00/give_bmi.py
@@ -0,0 +1,29 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    ## check if parameters are properly typed
+    assert len(height) > 0, "height array must contain at least one element"
+    assert len(weight) > 0, "weight array must contain at least one element"
+
+    ## add error templating for errors
+    ## check if there is both arrays are same length
+    assert len(height) == len(weight), "Arrays(height, weight) must be of the same length"
+
+    ## check for no null values or empty values 0 check proper typs inside list
+    assert all(height), "height array must not contain empty values"
+    assert all(weight), "weight array must not contain empty values"
+    # check if int and float
+    assert all(isinstance(i, int) or isinstance(i,float)  for i in height),"contents must be ints or floats"
+    assert all(isinstance(i, int) or isinstance(i,float)  for i in weight),"contents must be ints or floats" 
+
+
+    ## do math weight/height ^2
+    ## return array 
+    return [w/pow(h,2) for w,h in zip(weight,height)] 
+
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    ## this sounds weak need better checking
+    assert type(limit) is int, "Limit must be an int"
+    assert limit > 0, "Limit must be greater than 0"
+
+    return [ item > limit for item in bmi ] 
